fe7cd1721767963b83a616ab7d4e7d3c64b2a31e
Artemmm008 <artemshkola141@gmail.com>
2026-08-29 17:52:38 +0100

    fixed paymenRoute refund only for admin

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
-import { optionalAuthenticate } from "../middleware/authenticate.js";
+import { authenticate, optionalAuthenticate } from "../middleware/authenticate.js";
+import { checkRoles } from "../middleware/checkRoles.js"
 import { createPaymentSessionController, refundOrderController } from "../controllers/paymentController.js";
 import { createCheckoutSessionSchema, refundOrderSchema } from "../validations/paymentValidation.js";
 
 const router = Router();
 
 router.post("/payments/create-checkout-session", optionalAuthenticate, celebrate(createCheckoutSessionSchema), createPaymentSessionController);
-router.post("/payments/refund/:orderId", optionalAuthenticate, celebrate(refundOrderSchema), refundOrderController);
+router.post("/payments/refund/:orderId", authenticate, checkRoles("admin"), celebrate(refundOrderSchema), refundOrderController);
 export default router;
