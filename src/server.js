3a634bb1555a60d2a9f332a6472713f3677a1899
Artemmm008 <artemshkola141@gmail.com>
2026-09-05 15:14:17 +0100

    fixed cors

--- src/server.js
+++ src/server.js
@@ -15,7 +15,10 @@ import cookieParser from "cookie-parser"
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.post("/payments/webhook", express.raw({ type: "application/json" }), handlePaymentWebhook);
